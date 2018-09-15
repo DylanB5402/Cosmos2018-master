fcd25bec63378475f8620218a65c925e66d22abd
Dylan Barva <d.barva5402@gmail.com>
2018-09-14 21:19:24 -0700

    wrote pf code

--- src/main/java/org/usfirst/frc/team687/robot/constants/AutoConstants.java
+++ src/main/java/org/usfirst/frc/team687/robot/constants/AutoConstants.java
@@ -1,18 +1,21 @@
 package org.usfirst.frc.team687.robot.constants;
 
+import org.usfirst.frc.team687.robot.constants.DriveConstants;
+
 import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Trajectory.Config;
 import jaci.pathfinder.Waypoint;
 import jaci.pathfinder.Pathfinder;
-
+import jaci.pathfinder.modifiers.TankModifier;
 public class AutoConstants {
 
-    public static Config test_config = new Config(Trajectory.FitMethod.HERMITE_CUBIC, Config.SAMPLES_HIGH, 0.02, 13, 9, 100);
-    public static Waypoint[] test_points = new Waypoint[] {
+    private static Config test_config = new Config(Trajectory.FitMethod.HERMITE_CUBIC, Config.SAMPLES_HIGH, 0.02, 13, 9, 100);
+    private static Waypoint[] test_points = new Waypoint[] {
         new Waypoint(0, 0, Pathfinder.d2r(90)),
-        new Waypoint(5, 7, Pathfinder.d2r(90))
-        
+        new Waypoint(5, 7, Pathfinder.d2r(90))  
     };
-    public static Trajectory test = Pathfinder.generate(test_points, test_config);
+    private static Trajectory test = Pathfinder.generate(test_points, test_config);
+    
 
+    
 }
